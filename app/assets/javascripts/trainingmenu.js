4740a8cf86574c39921e01fcf0dfbb96aff9c587
kurachitoshiaki <aru0220kuria0214@gmail.com>
2019-11-18 18:10:44 +0900

    Update trainingmenu.js

--- app/assets/javascripts/trainingmenu.js
+++ app/assets/javascripts/trainingmenu.js
@@ -3,16 +3,16 @@
 // $(document).ready(function () {
 
 
-//     $('input[name="check-all"]').on('click', function() {
-//     $('input[type="checkbox"]').attr('checked', true).prop('checked', true).change();
+//     $('input[name='check-all']').on('click', function() {
+//     $('input[type='checkbox']').attr('checked', true).prop('checked', true).change();
 //     });
 
-//     $('input[name="uncheck-all"]').on('click', function(){
-//     $('input[type="checkbox"]').removeAttr('checked').prop('checked', false).change();
+//     $('input[name='uncheck-all']').on('click', function(){
+//     $('input[type='checkbox']').removeAttr('checked').prop('checked', false).change();
 //     });
 
 //     $('#training').on('submit', function (){
-//     var $checkbox = $('input[type="checkbox"]');
+//     var $checkbox = $('input[type='checkbox']');
 //     alert(
 //         $checkbox.attr('checked') || $checkbox.prop('checked') ?
 //         '登録が完了しました' : '選択してください'
